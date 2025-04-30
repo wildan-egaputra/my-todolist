8a2d89b1c1b4393d74e1add071f815a8a1c28ba7
Altheinzz <wildanegaputra@gmail.com>
2025-04-30 18:22:52 +0700

    memperbaiki form edit

--- edit.php
+++ edit.php
@@ -4,6 +4,7 @@ include 'koneksi.php';
 
 
 $id = $_GET['id_todo'];
+
 $sql = "SELECT*FROM todo where id_todo = $id";
 $qq = mysqli_query($konek,$sql);
 
@@ -23,20 +24,23 @@ $bar = mysqli_fetch_assoc($qq);
         <form action="proses_create.php" method="post">
             <div class="input">
                 <label for="judul">judul:</label>
-                <input type="text" name="judul" required>
+                <input type="text" name="judul" value="<?= $bar['judul']?>" required>
             </div>
             <div class="input">
                 <label for="deskripsi">deskripsi</label>
-                <input type="text" name="deskripsi" required>
+                <input type="text" name="deskripsi" value="<?= $bar['deskripsi']?>" required>
             </div>
             <select name="status">
                 <option value="pending"<?= $bar['status'] == "pending" ? 'selected' : ''?>>pending</option>
                 <option value="done"<?= $bar['status'] == "done" ? 'selected' : ''?>>done</option>
             </select>
             <select name="id_category">
-                <?php while($bar =mysqli_fetch_assoc($qq)){
+                <?php 
+                $sql2 = "SELECT*FROM category";
+                $qq2 = mysqli_query($konek,$sql2);
+                while($bar =mysqli_fetch_assoc($qq2)){
                     ?>
-                    <option value="<?= $bar['id_category']?>"> <?= $bar['category']?></option>
+                    <option value="<?= $bar['id_category']?>"<?= $bar['id_category'] == $bar['id_category'] ? "selected" : ''?> > <?= $bar['category']?></option>
                     <?php
                 }
                 ?>
